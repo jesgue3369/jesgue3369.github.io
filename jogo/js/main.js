8f0c4765cc6a600af86af35d0863fc7a98afaece
jesgue3369 <dilermando4444@gmail.com>
2025-06-20 00:16:25 -0300

    Delete jogo/js directory

--- jogo/js/main.js
+++
@@ -1,6 +0,0 @@
-// main.js
-import { initializeGame } from './gameLogic.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeGame('gameCanvas'); // Inicializa o jogo no canvas com o ID 'gameCanvas'
-});
