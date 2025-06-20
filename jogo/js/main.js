b527ea85fec986f19e9b272b0596fa2459955b40
jesgue3369 <dilermando4444@gmail.com>
2025-06-20 00:00:56 -0300

    Update main.js

--- jogo/js/main.js
+++ jogo/js/main.js
@@ -1,1 +1,6 @@
+// main.js
+import { initializeGame } from './gameLogic.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+    initializeGame('gameCanvas'); // Inicializa o jogo no canvas com o ID 'gameCanvas'
+});
